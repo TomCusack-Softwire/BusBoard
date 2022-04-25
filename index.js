c615e4ead2405e162fd7d79ac09b9a36bf4b6b7a
Tom Cusack <Tom.Cusack@softwire.com>
2022-04-25 14:17:18 +0100

    Part 2 Convert post code => lat/long => nearest bus stations, then print data for stations.

--- index.js
+++ index.js
@@ -8,26 +8,77 @@ try {
     api_key = "?app_key=" + fs.readFileSync("api_key", "utf-8");
 } catch (_) {}
 
-let user_input = readline.question("Enter stop ID: ");
-let url = "https://api.tfl.gov.uk/StopPoint/" + user_input + "/Arrivals" + api_key;
+function get_StopPoint_data(stop_id, callback, message="") {
+    let url = "https://api.tfl.gov.uk/StopPoint/" + stop_id + "/Arrivals" + api_key;
 
-request(url, (error, response, body) => {
-    let code = response && response.statusCode;
+    request(url, (error, response, body) => {
+        if (response && response.statusCode !== 200) {
+            console.error("Error accessing StopPoint stop.");
+        } else {
+            console.log("\n" + message);
+            let json = JSON.parse(body);
+            if (json.length > 0) {
+                for (let bus of json) {
+                    let minutes = (new Date(bus["expectedArrival"]) - new Date()) / 60000;
+                    bus["minutesNumber"] = minutes;
+                    if (minutes <= 0) {
+                        bus["minutes"] = "Due";
+                    } else {
+                        bus["minutes"] = Math.ceil(minutes) + " minutes";
+                    }
+                }
 
-    if (code === 404) {
-        console.log("Incorrect stop ID.");
-    } else {
-        let result = JSON.parse(body);
-        for (let bus of result) {
-            let minutes = (new Date(bus["expectedArrival"]) - new Date()) / 60000;
-            bus["minutesNumber"] = minutes;
-            if (minutes <= 0) {
-                bus["minutes"] = "Due";
+                json.sort((a, b) => a["minutesNumber"] - b["minutesNumber"]);
+                callback(json.slice(0, 5));
             } else {
-                bus["minutes"] = Math.ceil(minutes) + " minutes";
+                console.log("No buses scheduled right now.");
             }
         }
-        result.sort((a, b) => a["minutesNumber"] - b["minutesNumber"]);
-        console.table(result.slice(0, 5), ["lineName", "destinationName", "minutes"]);
+    });
+}
+
+function get_StopPoint_data_from_station(station, callback) {
+    if (station === undefined) {
+        return;
     }
+
+    get_StopPoint_data(
+        station["id"],
+        (result) => {
+            console.table(result, ["lineName", "destinationName", "minutes"]);
+            callback(result);
+        },
+        station["commonName"] + " (" + station["id"] + ", distance: " + station["distance"].toFixed(0) + "m)"
+    );
+}
+
+
+function get_lat_long(postcode, callback) {
+    let url = "https://api.postcodes.io/postcodes/" + postcode;
+    request(url, (error, response, body) => {
+        let json = JSON.parse(body);
+        if (json["status"] !== 200) {
+            console.error(json["error"]);
+        } else {
+            callback(json["result"]["latitude"], json["result"]["longitude"]);
+        }
+    });
+}
+
+function get_StopPoint_stations(lat, long, callback) {
+    let url = "https://api.tfl.gov.uk/StopPoint/?lat=" + lat + "&lon=" + long + "&stopTypes=NaptanPublicBusCoachTram&" + api_key.slice(1);
+    request(url, (error, response, body) => {
+        let stops = JSON.parse(body)["stopPoints"];
+        stops.sort((a, b) => a["distance"] - b["distance"]);
+        callback(stops);
+    });
+}
+
+let stop_id = readline.question("Enter postcode: ");
+get_lat_long(stop_id, (lat, long) => {
+    get_StopPoint_stations(lat, long, (stations) => {
+        get_StopPoint_data_from_station(stations[0], (result1) => {
+            get_StopPoint_data_from_station(stations[1], (result2) => {});
+        });
+    });
 });
