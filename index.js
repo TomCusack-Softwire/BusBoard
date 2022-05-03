9f6144c01cfc7b4e884934c5287903050854c5b1
Tom Cusack <Tom.Cusack@softwire.com>
2022-05-03 14:32:06 +0100

    Improved code based on comments from Emil :)

--- index.js
+++ index.js
@@ -6,99 +6,94 @@ const express = require("express");
 let api_key = "";
 try {
     api_key = "?app_key=" + fs.readFileSync("api_key", "utf-8");
-} catch (_) {}
+} catch (error) {
+    console.error("API key file (./api_key) doesn't exist. Continuing with rate-limited anonymous mode.");
+}
 
-function get_StopPoint_data(stop_id) {
-    let url = "https://api.tfl.gov.uk/StopPoint/" + stop_id + "/Arrivals" + api_key;
+// set classes for responses, instead of random objects
+class StopPointBus {
+    constructor(lineName, destinationName, expectedArrival) {
+        this.lineName = lineName;
+        this.destinationName = destinationName;
+        this.expectedArrival = expectedArrival;
+        this.minutes = (new Date(this.expectedArrival) - new Date()) / 60000;
+        this.formattedMinutes = this.minutes <= 0 ? "Due" : `${Math.ceil(this.minutes)} min${this.minutes <= 1 ? "" : "s"}`;
+    }
+}
 
+class StopPointStation {
+    constructor(id, commonName, distance) {
+        this.id = id;
+        this.commonName = commonName;
+        this.distance = distance.toFixed(0);
+    }
+}
+
+class Coordinate {
+    constructor(latitude, longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
+
+function getJSONFromURL(url) {
     return new Promise((resolve, reject) => {
         request(url, (error, response, body) => {
             if (response && response.statusCode !== 200) {
-                reject("Error accessing StopPoint stop.");
+                reject(`Error accessing URL: ${url.replace(/[&?]app_key=.*/g, "")}`);
             } else {
-                let json = JSON.parse(body);
-                if (json.length > 0) {
-                    for (let bus of json) {
-                        let minutes = (new Date(bus["expectedArrival"]) - new Date()) / 60000;
-                        bus["minutesNumber"] = minutes;
-                        if (minutes <= 0) {
-                            bus["minutes"] = "Due";
-                        } else if (minutes <= 1) {
-                            bus["minutes"] = "1 min";
-                        } else {
-                            bus["minutes"] = Math.ceil(minutes) + " mins";
-                        }
-                    }
-
-                    json.sort((a, b) => a["minutesNumber"] - b["minutesNumber"]);
-                    resolve(json.slice(0, 5));
-                } else {
-                    resolve("No buses scheduled right now.");
-                }
+                resolve(JSON.parse(body));
             }
         });
     });
 }
 
-function get_lat_long(postcode) {
-    let url = "https://api.postcodes.io/postcodes/" + postcode;
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            let json = JSON.parse(body);
-            if (json["status"] !== 200) {
-                reject(json["error"]);
-            } else {
-                resolve([json["result"]["latitude"], json["result"]["longitude"]]);
-            }
-        });
-    });
+function formatStopPointData(station, timetable) {
+    const header = `<h3 style="font-weight: normal;">
+            <b><a class="no-link" title="Click to go to the TFL website for this stop." href="https://tfl.gov.uk/bus/stop/${station.id}"> ${station.commonName} </a></b>
+            (${station.distance}m away)
+        </h3>
+    `;
+
+    const timetableString = timetable.length === 0 ?
+        "No buses are scheduled right now." :
+        "<table>" + timetable.map(bus => `<tr><td>${bus.lineName}</td> <td>${bus.destinationName}</td> <td>${bus.formattedMinutes}</td></tr>`).join("") + "</table>";
+
+    return header + timetableString;
 }
 
-function get_StopPoint_stations(lat, long) {
-    let url = "https://api.tfl.gov.uk/StopPoint/?lat=" + lat + "&lon=" + long + "&stopTypes=NaptanPublicBusCoachTram&" + api_key.slice(1);
-    return new Promise((resolve) => {
-        request(url, (error, response, body) => {
-            let stops = JSON.parse(body)["stopPoints"];
-            stops.sort((a, b) => a["distance"] - b["distance"]);
-            resolve(stops);
-        });
-    });
-}
-
-function get_data_from_station(station) {
-    if (station === undefined) {
-        return;
-    }
-
-    return get_StopPoint_data(station["id"])
-        .then((timetable) => {
-            let message = "<h3 style='font-weight: normal;'><b><a class='no-link' title='Click to go to the TFL website for this stop.' href='https://tfl.gov.uk/bus/stop/";
-            message += station["id"] + "'>" + station["commonName"] + "</a></b> (" + station["distance"].toFixed(0) + "m away)</h3>\n<table>\n";
-            for (let bus of timetable) {
-                message += "<tr><td>" + bus["lineName"] + "</td><td>" + bus["destinationName"] + "</td><td>" + bus["minutes"] + "</td></tr>\n";
-            }
-            message += "</table>";
-            return message;
+function getStopPointDataFromStation(station) {
+    return getJSONFromURL(`https://api.tfl.gov.uk/StopPoint/${station.id}/Arrivals${api_key}`)
+        .then(json => {
+            const buses = json.map(bus => new StopPointBus(bus.lineName, bus.destinationName, bus.expectedArrival));
+            buses.sort((a, b) => a.minutes - b.minutes);
+            return formatStopPointData(station, buses.slice(0, 5));
         });
 }
 
-function postcode_to_timetable(postcode) {
-    return get_lat_long(postcode)
-        .then((result) => {
-            return get_StopPoint_stations(result[0], result[1]);
-        })
-        .then((stations) => {
-            return Promise.all([get_data_from_station(stations[0]), get_data_from_station(stations[1])])
-                .then((values) => {
-
-                    if (values[0] === undefined && values[1] === undefined) {
-                        return "No stations within 200m of this postcode.";
-                    } else {
-                        return values.join("");
-                    }
-
-                });
+function getCoordinatesFromPostcode(postcode) {
+    return getJSONFromURL(`https://api.postcodes.io/postcodes/${postcode}`)
+        .then(json => new Coordinate(json.result.latitude, json.result.longitude))
+        .catch(error => {
+            throw `Not a valid postcode: ${postcode}`;
         });
+}
+
+function getStopPointStations(coordinate) {
+    return getJSONFromURL(`https://api.tfl.gov.uk/StopPoint/?lat=${coordinate.latitude}&lon=${coordinate.longitude}&stopTypes=NaptanPublicBusCoachTram&${api_key.slice(1)}`)
+        .then(json => {
+            const stations = json.stopPoints.map(stop => new StopPointStation(stop.id, stop.commonName, stop.distance));
+            stations.sort((a, b) => a.distance - b.distance);
+            return stations;
+        });
+}
+
+function postcodeToTimetable(postcode) {
+    return getCoordinatesFromPostcode(postcode)
+        .then(coordinate => getStopPointStations(coordinate))
+        .then(stations => Promise.all(stations.slice(0, 2).map(getStopPointDataFromStation))
+            .then(values => values.length === 0 ? "No stations within 200m of this postcode." : values.join(""))
+        );
 }
 
 const app = express();
@@ -110,10 +105,10 @@ app.listen(3000, () => {
 });
 
 app.get("/departureBoards", (request, response) => {
-    if (request.query["postcode"]) {
-        postcode_to_timetable(request.query["postcode"])
-            .then((message) => response.send(message))
-            .catch((error) => response.status(400).send(error));
+    if (request.query.postcode) {
+        postcodeToTimetable(request.query.postcode)
+            .then(message => response.send(message))
+            .catch(error => response.status(400).send(error));
     } else {
         response.send("Please enter a postcode.");
     }
